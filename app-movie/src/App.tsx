848a086413b661d6b3e2d77f2bf17028e0c6f0a2
ThuySeven23 <huuthuytrann3004@gmail.com>
2025-01-30 00:10:28 +0700

    project-movie-app

---
+++ app-movie/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-noto underline text-blue-400">Hello Tailwind CSS!</h1>
+    </>
+  );
+}
+
+export default App;
